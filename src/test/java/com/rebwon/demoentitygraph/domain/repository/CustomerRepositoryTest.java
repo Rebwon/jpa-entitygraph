2c59eb775127f74063e2224019a4204663a9e398
Rebwon <msolo021015@naver.com>
2020-04-21 19:21:20 +0900

    refactor code

--- src/test/java/com/rebwon/demoentitygraph/domain/repository/CustomerRepositoryTest.java
+++ src/test/java/com/rebwon/demoentitygraph/domain/repository/CustomerRepositoryTest.java
@@ -1,13 +1,8 @@
 package com.rebwon.demoentitygraph.domain.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import com.rebwon.demoentitygraph.domain.Customer;
 
 @DataJpaTest
 class CustomerRepositoryTest {
